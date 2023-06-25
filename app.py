0a5c2af892020add132659dcb3fcb04982b27219
Almira Mahsa <almiramahsa9@gmail.com>
2023-06-25 10:25:48 +0700

    'test'

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta, datetime
+import jwt
 
 app = Flask(__name__)
 secret_key = 'qwertyasd'
@@ -21,8 +23,24 @@ def register():
             {'username': data['username'], 'password': generate_password_hash(data['password'])})
         return jsonify({'message': "User registered successfully!"})
     else:
-        return jsonify({'message': 'Invalid data'})
+        return jsonify({'message': 'Data Empty'})
+
+
+@app.route("/api/auth/login", methods=['POST'])
+def login():
+    data = request.form.to_dict()
+    user = mongo.db['user'].find_one({'username': data['username']})
+
+    if not user or not check_password_hash(user['password'], data['password']):
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    payload = {'_id': str(user['_id']),
+               'exp': datetime.utcnow() + timedelta(minutes=30)}
+    access_token = jwt.encode(
+        payload, secret_key, algorithm='HS256').decode('utf-8')
+
+    return jsonify({'token': token}), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
